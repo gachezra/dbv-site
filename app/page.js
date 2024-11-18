dabd1652622c0a704f1a14f0646cc4e65226ad03
Ezra Kipkoech <71008423+gachezra@users.noreply.github.com>
2024-11-18 17:12:51 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 
 import Header from './components/Header';
 import Footer from './components/Footer';
-import QuotationCart from './QuotationCart';
+import QuotationCart from './catalog/QuotationCart';
 import HeroSection from './components/HeroSection';
 import ReviewsSection from './components/ReviewsSection';
 import FeaturedProducts from './components/FeaturedProducts';
